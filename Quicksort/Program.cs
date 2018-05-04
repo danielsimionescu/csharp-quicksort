e0fd50b766afa00b02294ef3789bf828225c2f35
Daniel <danielsimionescu1996@gmail.com>
2018-05-04 11:35:17 +0300

    Implemented generic Quicksort

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -7,47 +7,60 @@ namespace Quicksort
         static void Main(string[] args)
         {
             var numbers = new[] { 1, 5, 0, 34, 3, 9 };
+            var letters = new[] { 'f', 'g', 'a', 'm', 'o' };
+            var names = new[] { "Peter", "Alex", "John", "Fred" };
+            var books = new[] { new Book("4ds"), new Book("2er"), new Book("31") };
 
             // Quicksort
             SortArray(numbers);
+            SortArray(letters);
+            SortArray(names);
+            SortArray(books);
             //
 
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
-            }
+            PrintArray(numbers);
+            PrintArray(letters);
+            PrintArray(names);
+            PrintArray(books);
 
             Console.WriteLine("\nPress any key to stop execution.");
             Console.ReadKey();
         }
 
-
-
-        private static void SortArray(int[] numbers)
+        public static void PrintArray<T>(T[] array)
         {
-            Quicksort(numbers, 0, numbers.Length - 1);
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
         }
 
-        private static void Quicksort(int[] numbers, int left, int right)
+        private static void SortArray<T>(T[] array) where T : IComparable<T>
+        {
+            Quicksort(array, 0, array.Length - 1);
+        }
+
+        private static void Quicksort<T>(T[] array, int left, int right) where T : IComparable<T>
         {
             int i = left;
             int j = right;
 
-            var pivot = numbers[left + (right - left) / 2];
+            var pivot = array[left + (right - left) / 2];
 
-            while(i <= j)
+            while (i <= j)
             {
-                while (numbers[i] < pivot)
+                while (array[i].CompareTo(pivot) < 0)
                     i++;
 
-                while (numbers[j] > pivot)
+                while (array[j].CompareTo(pivot) > 0)
                     j--;
 
-                if(i <= j)
+                if (i <= j)
                 {
-                    var tmp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = tmp;
+                    var tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
 
                     i++;
                     j--;
@@ -55,10 +68,30 @@ namespace Quicksort
             }
 
             if (left < j)
-                Quicksort(numbers, left, j);
+                Quicksort(array, left, j);
 
             if (i < right)
-                Quicksort(numbers, i, right);
+                Quicksort(array, i, right);
+        }
+    }
+
+    class Book : IComparable<Book>
+    {
+        public string ISBN { get; set; }
+
+        public Book(string isbn)
+        {
+            ISBN = isbn;
+        }
+
+        public int CompareTo(Book other)
+        {
+            return ISBN.CompareTo(other.ISBN);
+        }
+
+        public override string ToString()
+        {
+            return ISBN;
         }
     }
 }
